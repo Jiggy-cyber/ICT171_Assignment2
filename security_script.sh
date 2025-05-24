c5c23480575c5351a55b51414b4f813160ff3fdb
Jiggy-cyber <jkuendrol@gmail.com>
2025-05-24 15:11:00 +0800

    Update security_script.sh

--- security_script.sh
+++ security_script.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
+#Updates the package list(apache is now available)
 sudo apt update
+
+#Installs Apache2 web server
 sudo apt install apache2
+
+#Enables the firewall
 sudo ufw enable
+
+#Installs certbot for SSL certificate
 sudo apt install certbot python3-certbot-apache 
+
+#Runs Certbot to configure Apache with the SSL certificate
+#Requires the user to enter their email address and domain name
 sudo certbot --apache
